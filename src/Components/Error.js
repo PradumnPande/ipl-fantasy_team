23bc9e21a5682a702c1758995677749870e06944
bidyut97 <bidyut97tarai@gmail.com>
2022-05-12 23:40:37 +0530

    HOME

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import image from './Components/wrong.jpg';
  class Error extends Component {
   render() {
     return (
       <div>
           <h1>SORRY!SOMETHING WENT WRONG</h1>
-          
+          <img src={image} alt=" wrong"/>
           </div>
     )
   }
