b92370041c20a64db45b7dcdb94d74df9a2e90ca
PradumnPande <pradumn-dipak.pande@capgemini.com>
2022-05-13 11:36:50 +0530

    Admin

---
+++ src/Component/AdminRegisterSuccess.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AdminRegisterSuccess extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Admin Registered Successfully</h1>
+            </div>
+        )
+    }
+}
+
+export default AdminRegisterSuccess;
