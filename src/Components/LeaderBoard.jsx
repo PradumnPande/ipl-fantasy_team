6bc36ea447aac4087a3265c1201b2080a3cad43d
SAI KRISHNA <saikrish635@gmail.com>
2022-05-16 20:21:28 +0530

    leaderboard

--- src/Components/LeaderBoard.jsx
+++ src/Components/LeaderBoard.jsx
@@ -2,12 +2,23 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 
-function LeaderBoard() {
-  const [users, setUsers] = useState([])
+function Biddings() {
+  const [biddings, setbiddings] = useState([])
+  const userid = sessionStorage.getItem('id')
+  const role = sessionStorage.getItem('role')
+
+  //load data at startup
   const loadData = () => {
-    axios.get('http://localhost:8080/api/users').then((resp) => {
-      setUsers(resp.data)
-    })
+    if (role === 'User')
+      axios
+        .get('http://localhost:8080/api/bidders/bids?bidderid=' + userid)
+        .then((resp) => {
+          setbiddings(resp.data)
+        })
+    else
+      axios.get('http://localhost:8080/api/bidders/bids').then((resp) => {
+        setbiddings(resp.data)
+      })
   }
 
   useEffect(() => {
@@ -20,28 +31,36 @@ function LeaderBoard() {
         <div className='row'>
           <div className='col-sm-12'>
             <h4 className='text-left p-2 border-bottom border-success'>
-              All Users
+              Biddings
             </h4>
-            <table className='table table-bordered table-light table-hover'>
+            <table className='table table-bordered table-light table-sm'>
               <thead>
                 <tr>
                   <th>Id</th>
-                  <th>Name</th>
-                  <th>Gender</th>
-                  <th>Password</th>
-                  <th>Userid</th>
-                  <th>Role</th>
+                  <th>Match</th>
+                  <th>Opinion</th>
+                  <th>Result</th>
+                  <th>Win/Loss</th>
+                  <th>Bidder</th>
                 </tr>
               </thead>
               <tbody>
-                {users.map((x) => (
+                {biddings.map((x) => (
                   <tr key={x.id}>
                     <td>{x.id}</td>
-                    <td>{x.uname}</td>
-                    <td>{x.gender}</td>
-                    <td>{x.password}</td>
-                    <td>{x.userid}</td>
-                    <td>{x.admin ? 'Admin' : 'User'}</td>
+                    <td>
+                      {x.match.teamOne.tname + ' vs ' + x.match.teamTwo.tname}
+                    </td>
+                    <td>{x.opinion}</td>
+                    <td>{x.result}</td>
+                    <td>
+                      {x.result && x.result == x.opinion
+                        ? 'Win'
+                        : x.result && x.result != x.opinion
+                        ? 'Loss'
+                        : null}
+                    </td>
+                    <td>{x.bidder.name}</td>
                   </tr>
                 ))}
               </tbody>
@@ -53,4 +72,4 @@ function LeaderBoard() {
   )
 }
 
-export default LeaderBoard
+export default Biddings
